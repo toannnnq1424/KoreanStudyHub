b197e9101fcd4479ec97ad3346e5a8c61285245d
Nguyễn Quang Toàn <toanlamsaoduocc@MacBook-Air-cua-Nguyen.local>
2026-06-08 21:53:31 +0700

    Initial commit: Set up Spring Boot project with package structure

---
+++ src/main/java/org/example/sep490/koreanstudyhub/KoreanStudyHubApplication.java
@@ -0,0 +1,15 @@
+package org.example.sep490.koreanstudyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class KoreanStudyHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreanStudyHubApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/org/example/sep490/koreanstudyhub/KoreanStudyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sep490.koreanstudyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreanStudyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
